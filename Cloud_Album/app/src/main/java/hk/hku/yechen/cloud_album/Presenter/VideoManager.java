d665e199a94ea30a37bc27e8f53a26bb676edd2d
fdfangbaby <fangbaby93@gmail.com>
2016-11-28 15:18:42 +0800

    able to receive json

--- Cloud_Album/app/src/main/java/hk/hku/yechen/cloud_album/Presenter/VideoManager.java
+++ Cloud_Album/app/src/main/java/hk/hku/yechen/cloud_album/Presenter/VideoManager.java
@@ -1,10 +1,15 @@
 package hk.hku.yechen.cloud_album.Presenter;
 
+import android.util.Log;
+
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.List;
@@ -15,17 +20,23 @@ import hk.hku.yechen.cloud_album.Model.Album;
  */
 
 public class VideoManager implements Runnable {
-    List<Album> albums;
+
+    private  List<Album> albums;
+
+
+
+    private static String urlPath  = "https://i.cs.hku.hk/~cfang/app/getlist.php";
 
     public VideoManager(List albums){
         this.albums = albums;
     }
+
     public void getDataFromServer() throws Exception{
-        String path  = "http://i.cs.hku.hk/~cfang/test/upload.php";
-        URL url = new URL(path);
+
+        URL url = new URL(urlPath);
         HttpURLConnection con = (HttpURLConnection) url.openConnection();
         con.setConnectTimeout(5000);// 设置超时时间
-        con.setRequestMethod("GET");// 设置请求方式
+        con.setRequestMethod("POST");// 设置请求方式
         if(con.getResponseCode() == 200){
             InputStream inStream = con.getInputStream();
             parseJSON(inStream);
@@ -33,26 +44,27 @@ public class VideoManager implements Runnable {
     }
 
     private void parseJSON (InputStream inStream) throws Exception{
-        byte[] data = read(inStream);
-        String json = new String(data);
+        String json = convertStreamToString(inStream);
+        Log.d("data:",json);
         JSONArray array = new JSONArray(json);
         for (int i = 0;i<array.length();i++){
             JSONObject jsonObject = array.getJSONObject(i);
             Album album = new Album (jsonObject.getInt("id"),jsonObject.getString("name"),jsonObject.getString("timestamp"));
+            Log.d("video","id:"+album.getId()+"name:"+album.getName()+"time:"+album.getTimestamp());
             albums.add(album);
         }
     }
 
-    private byte[] read (InputStream inputStream) throws Exception{
-        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-        byte[] b = new byte[1024];
-        int len = 0;
-        while ((len = inputStream.read(b))!=-1){
-            outputStream.write(b,0,len);
-        }
-        inputStream.close();
-        return outputStream.toByteArray();
-    }
+//    private byte[] read (InputStream inputStream) throws Exception{
+//        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+//        byte[] b = new byte[1024];
+//        int len = 0;
+//        while ((len = inputStream.read(b))!=-1){
+//            outputStream.write(b,0,len);
+//        }
+//        inputStream.close();
+//        return outputStream.toByteArray();
+//    }
 
     public void postVideoToServer(){
 
@@ -60,6 +72,27 @@ public class VideoManager implements Runnable {
 
     public List<Album> getAlbums(){
         return albums;
+    }
+
+    private String convertStreamToString(InputStream is) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        StringBuilder sb = new StringBuilder();
+
+        String line = null;
+        try {
+            while ((line = reader.readLine()) != null) {
+                sb.append(line).append('\n');
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                is.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return sb.toString();
     }
 
     @Override
